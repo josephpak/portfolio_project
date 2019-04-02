6cc2b93302164f4d30b74638c2bd90b5d6bd9c6c
joepak <josephpak2014@gmail.com>
2019-04-02 16:53:22 -0400

    Make change to structure for deploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 
-import Header from "../Header/Header"
-import "../reset.css"
+import Header from "./Header/Header"
+import "./reset.css"
 
 import {
   BodyWrapper,
   FooterWrapper
-} from "./LayoutStyles"
+} from "./Layout/LayoutStyles"
 
 const Layout = ({ children }) => (
   <StaticQuery
--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,7 +1,7 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/Layout/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 // import styled from 'styled-components'
 
