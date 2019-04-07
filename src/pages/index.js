f5d8203ba6c7082697e214e81b879eb8d1b42dcb
joepak <josephpak2014@gmail.com>
2019-04-06 23:41:50 -0400

    Testing alias

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 // import { Link } from "gatsby"
 import WorkTimeline from "../components/Timeline/WorkTimeline"
 import Layout from "../components/layout"
-import climb from "../images/climb.jpg"
+import rocks from "../images/rocks.jpg"
 
 import SEO from "../components/seo"
 
@@ -32,10 +32,9 @@ const AboutMe = () => (
             <h1>Contact me</h1>
             <h2>josephpak2014@gmail.com</h2>
             <hr />
-            
           </ContactCardLeft>
           <ContactCardRight>
-            <img src={climb}/>
+            <img src={rocks}/>
           </ContactCardRight>
         </ContactContentCard>
       </ContactContentWrapper>
